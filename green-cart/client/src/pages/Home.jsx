d116285c2903368bb4e349faa0d4fce6293b125a
Girish <girish.soman@schbang.com>
2025-04-12 19:48:25 +0530

    feat: add BestSeller component to Home page for showcasing top-selling products

--- green-cart/client/src/pages/Home.jsx
+++ green-cart/client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
 import CategoriesList from '../components/CategoriesList'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <MainBanner />
         <CategoriesList />
+        <BestSeller />
     </div>
   )
 }
