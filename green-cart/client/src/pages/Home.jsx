f42b1bb29afd617ca51269b660bf41633d081ff4
Girish <girish.soman@schbang.com>
2025-04-14 18:21:59 +0530

    feat: add NewsLetter component to Home page layout

--- green-cart/client/src/pages/Home.jsx
+++ green-cart/client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner'
 import CategoriesList from '../components/CategoriesList'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <CategoriesList />
         <BestSeller />
         <BottomBanner />
+        <NewsLetter />
     </div>
   )
 }
