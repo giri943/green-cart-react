4d4dee34878fd6e9ad1267598f6b38ceeb5ec0fe
Girish <girish.soman@schbang.com>
2025-04-14 18:16:22 +0530

    feat: update grid layout in BestSeller component for improved responsiveness

--- green-cart/client/src/components/BestSeller.jsx
+++ green-cart/client/src/components/BestSeller.jsx
@@ -8,7 +8,7 @@ const BestSeller = () => {
     return (
         <div className='mt-16'>
             <p className='text-2xl md:text-3xl font-medium'>BestSeller</p>
-            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-6 mt-6'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:gird-cols-5 mt-6 '>
                 {products.filter((product) => product.category ==='Vegetables' || product.category ==='Fruits').map((product, index) => (
                     <ProductCard key={index} product={product} />
                 ))}
