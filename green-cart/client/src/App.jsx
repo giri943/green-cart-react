cb2ba06af2a3271304842a5ee3338034b9c313b7
Girish <girish.soman@schbang.com>
2025-04-14 21:14:50 +0530

    feat: integrate Login component based on user context for conditional rendering

--- green-cart/client/src/App.jsx
+++ green-cart/client/src/App.jsx
@@ -4,12 +4,18 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
+import Login from './components/Login'
+import { useAppContext } from './context/AppContext'
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes('/seller')
+  const {showUserLogin} = useAppContext();
+  console.log(showUserLogin);
+  
   return (
     <div>
       {isSellerPath ? null :<Navbar />}
+      {!showUserLogin ?  null : <Login />}
       <Toaster />
       <div className={`${isSellerPath ? '' : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}>
         <Routes>
